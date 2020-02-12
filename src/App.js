6ce86fc472a5b533f7a9b9553e1846385f89adf6
joegpdx <joegpdx@gmail.com>
2020-02-12 12:06:13 -0800

    final

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import './App.css';
 
 export default class App extends Component {
     state = {
-      selected: null
+      selected: null,
+      horns: null
     }
   render() {
       const handleChange = e => {
         console.log(e.target)
       this.setState({ selected: e.target.value });
+      }
+      const handleHornChange = e => {
+        console.log(e.target)
+      this.setState({ horns: e.target.value });
       }
 
     return (
@@ -22,29 +27,29 @@ export default class App extends Component {
              <option value="" defaultValue>
               All Types
              </option>
-              <option value="narwhal">Uniwhal</option>
-              <option value="rhino">Rhino Family</option>
-              <option value="unicorn">Unicorn Head</option>
-              <option value="unilego">Unilego</option>
-              <option value="narwhal">Basically a unicorn</option>
-              <option value="narwhal">#truth</option>
-              <option value="rhino">Baby Rhino</option>
-              <option value="triceratops">Cera</option>
-              <option value="narwhal">Narwhal costume</option>
-              <option value="rhino">Rhino costume</option>
-              <option value="unicorn">Believe</option>
-              <option value="markhor">Markhor</option>
-              <option value="markhor">Baby Markhor</option>
-              <option value="addax">Mouflon</option>
-              <option value="keyword">Addax</option>
-              <option value="chameleon">Happy Jackson's Chameleon</option>
-              <option value="chameleon">Serious Jackson's Chameleon</option>
-              <option value="lizard">Horned Lizard</option>
-              <option value="keyword">XyZ</option>
+              <option value="narwhal">narwhal</option>
+              <option value="rhino">rhino</option>
+              <option value="unicorn">unicorn</option>
+              <option value="unilego">unilego</option>
+              <option value="triceratops">triceratops</option>
+              <option value="markhor">markhor</option>
+              <option value="mouflon">mouflon</option>
+              <option value="chameleon">chameleon</option>
+              <option value="lizard">lizard</option>
+              <option value="dragon">dragon</option>
+           </select>
+           <select className="image-horns-filter" onChange={handleHornChange}>
+             <option value="test" defaultValue>
+              All Types
+             </option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="100">100</option>
            </select>
          </section>
         < section className="list-section">
-          <ImageList selected={this.state.selected} />
+          <ImageList selected={this.state.selected} horns={this.state.horns}/>
         </section>
        </main>
       </div>
--- src/ImageList.js
+++ src/ImageList.js
@@ -4,13 +4,16 @@ import ImageItem from "./ImageItem.js";
 
 export default class ImageList extends Component {
     render() {
-        console.log(this.props.selected)
+        console.log(this.props)
         const imageNodes = images
         .filter(image => {
         if (!this.props.selected) return true;
-
-        return image.keyword === this.props.selected;
+        return (image.keyword === this.props.selected);
     })
+        .filter(image => {
+                if (!this.props.horns) return true;
+                return (image.horns === Number(this.props.horns));   
+        })
         .map(image => <ImageItem image={image} />);
         return(
             <ul>
